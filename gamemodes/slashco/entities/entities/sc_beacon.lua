bce153fa782099d244f657614958e93f9fa498bd
textstack <46581273+textstack@users.noreply.github.com>
2023-03-29 20:12:14 -0700

    fix beacon ping

--- gamemodes/slashco/entities/entities/sc_beacon.lua
+++ gamemodes/slashco/entities/entities/sc_beacon.lua
@@ -12,7 +12,7 @@ ENT.Contact			= ""
 ENT.Purpose			= "A personal emergency terminal."
 ENT.Instructions	= ""
 ENT.IsSelectable 	= true
-ENT.PingPing = "DISTRESS BEACON"
+ENT.PingType = "ITEM"
 
 function ENT:Initialize()
 	if SERVER then
