f7680563d7552f48a8d02824b3d99f736d4c81ba
textstack <46581273+textstack@users.noreply.github.com>
2023-08-03 18:13:48 -0700

    position labmeat and flare properly

--- lua/slashco/item/flare.lua
+++ lua/slashco/item/flare.lua
@@ -15,11 +15,11 @@ ITEM.OnUse = function(ply)
     Entity( flare ):SetNWString("FlareDropperName", ply:Nick())
     SlashCo.CurRound.Items[flare] = true
 end
-ITEM.OnDrop = function(ply)
+ITEM.OnDrop = function()
 end
 ITEM.ViewModel = {
     model = "models/slashco/items/flare.mdl",
-    pos = Vector(65, 0, -5),
+    pos = Vector(64, 2, -5),
     angle = Angle(120, -120, -80),
     size = Vector(0.5, 0.5, 0.5),
     color = color_white,
@@ -30,10 +30,10 @@ ITEM.ViewModel = {
 }
 ITEM.WorldModelHolstered = {
     model = "models/slashco/items/flare.mdl",
-    bone = "ValveBiped.Bip01_R_Foot",
-    pos = Vector(2.5, 3, -0.2),
-    angle = Angle(0, -33, 90),
-    size = Vector(1.3, 1.3, 1.3),
+    bone = "ValveBiped.Bip01_Pelvis",
+    pos = Vector(5, 2, 5),
+    angle = Angle(100, -80, 0),
+    size = Vector(1, 1, 1),
     color = color_white,
     surpresslightning = false,
     material = "",
@@ -44,8 +44,8 @@ ITEM.WorldModel = {
     holdtype = "slam",
     model = "models/slashco/items/flare.mdl",
     bone = "ValveBiped.Bip01_R_Hand",
-    pos = Vector(4, 6, -1),
-    angle = Angle(180, 90, 0),
+    pos = Vector(3.2, 2, -1),
+    angle = Angle(200, 85, 0),
     size = Vector(1, 1, 1),
     color = color_white,
     surpresslightning = false,
