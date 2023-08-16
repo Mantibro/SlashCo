7aa21867ae7e762cc72b01855e7d751fe1a6c933
textstack <46581273+textstack@users.noreply.github.com>
2023-08-16 04:42:50 -0700

    fix nil error for info_sc_settings, clarifications in fgd

--- gamemodes/slashco/entities/entities/info_sc_settings/init.lua
+++ gamemodes/slashco/entities/entities/info_sc_settings/init.lua
@@ -11,25 +11,27 @@ function ENT:KeyValue(key, value)
 		self:StoreOutput(key, value)
 		return
 	end
-	if tonumber(value) < 0 then
+
+	local valNum = tonumber(value)
+	if valNum and valNum < 0 then
 		return
 	end
 
 	local key1 = string.lower(key)
 	if key1 == "generators_needed" then
-		SetGlobal2Int("SlashCoGeneratorsNeeded", tonumber(value))
+		SetGlobal2Int("SlashCoGeneratorsNeeded", valNum)
 		return
 	end
 	if key1 == "generators_spawned" then
-		SetGlobal2Int("SlashCoGeneratorsToSpawn", tonumber(value))
+		SetGlobal2Int("SlashCoGeneratorsToSpawn", valNum)
 		return
 	end
 	if key1 == "gascans_needed" then
-		SetGlobal2Int("SlashCoGasCansPerGenerator", tonumber(value))
+		SetGlobal2Int("SlashCoGasCansPerGenerator", valNum)
 		return
 	end
 	if key1 == "gascans_spawned" then
-		SetGlobal2Int("SlashCoGasCansToSpawn", tonumber(value))
+		SetGlobal2Int("SlashCoGasCansToSpawn", valNum)
 		return
 	end
 end
@@ -44,25 +46,27 @@ function ENT:AcceptInput(name, activator, _, value)
 	if SlashCo and SlashCo.RoundStarted then
 		return
 	end
-	if tonumber(value) < 0 then
+
+	local valNum = tonumber(value)
+	if valNum and valNum < 0 then
 		return
 	end
 
 	local name1 = string.lower(name)
 	if name1 == "set_generators_needed" then
-		SetGlobal2Int("SlashCoGeneratorsNeeded", tonumber(value))
+		SetGlobal2Int("SlashCoGeneratorsNeeded", valNum)
 		return true
 	end
 	if name1 == "set_generators_spawned" then
-		SetGlobal2Int("SlashCoGeneratorsToSpawn", tonumber(value))
+		SetGlobal2Int("SlashCoGeneratorsToSpawn", valNum)
 		return true
 	end
 	if name1 == "set_gascans_needed" then
-		SetGlobal2Int("SlashCoGasCansPerGenerator", tonumber(value))
+		SetGlobal2Int("SlashCoGasCansPerGenerator", valNum)
 		return true
 	end
 	if name1 == "set_gascans_spawned" then
-		SetGlobal2Int("SlashCoGasCansToSpawn", tonumber(value))
+		SetGlobal2Int("SlashCoGasCansToSpawn", valNum)
 		return true
 	end
 end
