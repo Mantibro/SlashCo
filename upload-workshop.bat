74cb0141bf0523aba5af880962d1cc8ad31d196f
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2025-04-08 12:26:43 +0200

    gamemode: change back workshop id & remove this stuff for the pr

--- upload-workshop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder "_workshop/" -out "_workshop.gma"
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -id "3453013573" -addon "_workshop.gma"
-del _workshop.gma
-pause
