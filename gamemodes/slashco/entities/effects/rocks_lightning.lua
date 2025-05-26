c2d35c23b6b18d6297e60307157efa6bab81b774
RaphaelIT7 <raphael.i.t@outlook.de>
2025-05-26 22:50:39 +0200

    repo: add a changelog to document all changes

--- gamemodes/slashco/entities/effects/rocks_lightning.lua
+++ gamemodes/slashco/entities/effects/rocks_lightning.lua
@@ -1,14 +1,10 @@
 local mat = Material("cable/blue_elec")
 function EFFECT:Init(data)
-	self.StartPos 	= data:GetStart()
-
-	self.HitPos 	= data:GetOrigin()
-
+	self.StartPos = data:GetStart()
+	self.HitPos = data:GetOrigin()
 	self.fDelta = 3
-
 	self.Entity:SetRenderBoundsWS(self.StartPos, self.HitPos)
 	self.BeamSize = 16
-
 	self.DieTime = CurTime() + math.Rand(0.5,1.0)
 end
 
@@ -23,51 +19,48 @@ function EFFECT:Render()
 
 	local start_pos = self.StartPos
 	local end_pos = self.HitPos
-	local dir = (end_pos - start_pos);
-	local increment = dir:Length() / 12;
-	dir:Normalize();
-	 
+	local dir = (end_pos - start_pos)
+	local increment = dir:Length() / 12
+	dir:Normalize()
+
 	// set material
-	render.SetMaterial(mat);
-	 
+	render.SetMaterial(mat)
+
 	// start the beam with 14 points
-	render.StartBeam(14);
-	 
+	render.StartBeam(14)
+
 	// add start
 	render.AddBeam(
 		start_pos,				// Start position
 		self.BeamSize,					// Width
 		CurTime(),				// Texture coordinate
 		Color(143, 167, 240, 255)		// Color
-	);
-	 
-	//
-	local i;
+	)
+	
 	for i = 1, 12 do
 		// get point
-		local point = (start_pos + dir * (i * increment)) + VectorRand() * math.random(1, 8);
-	 
+		local point = (start_pos + dir * (i * increment)) + VectorRand() * math.random(1, 8)
+
 		// texture coords
-		local tcoord = CurTime() + (1 / 12) * i;
-	 
+		local tcoord = CurTime() + (1 / 12) * i
+
 		// add point
 		render.AddBeam(
 			point,
 			self.BeamSize,
 			tcoord,
 			Color(143, 167, 240, 255)
-		);
-	 
+		)
 	end
-	 
+
 	// add the last point
 	render.AddBeam(
 		end_pos,
 		self.BeamSize,
 		CurTime() + 1,
 		Color(143, 167, 240, 255)
-	);
-	 
+	)
+
 	// finish up the beam
-	render.EndBeam();
+	render.EndBeam()
 end
