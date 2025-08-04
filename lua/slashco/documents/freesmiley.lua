4bd14fead2fb7d184de311c959800794844966ac
Xerkdot <152566871+Xerkdot@users.noreply.github.com>
2025-08-03 22:36:39 -0300

    Added information on two slasher documents

--- lua/slashco/documents/freesmiley.lua
+++ lua/slashco/documents/freesmiley.lua
@@ -4,7 +4,7 @@ DOCUMENT.Name = "Free Smiley Dealer"
 DOCUMENT.Type = "Slasher"
 DOCUMENT.Slasher = "FreeSmiley"
 
-DOCUMENT.Description = [[Awaiting Scout report]]
-DOCUMENT.AdditionalDescription = [[Awaiting Scout report]]
+DOCUMENT.Description = [[A cryptid entity that represents the appearance of a yellow tall guy with a big smile, it is said that he roams endlessly laughing near residential areas.]]
+DOCUMENT.AdditionalDescription = [[We have reports of him being followed by two floating creatures that will try to harm anyone that cross they line of sight]]
 
 SlashCo.RegisterDocument(DOCUMENT)
--- lua/slashco/documents/leuonard.lua
+++ lua/slashco/documents/leuonard.lua
@@ -3,7 +3,7 @@ local DOCUMENT = {}
 DOCUMENT.Name = "Leuonard"
 DOCUMENT.Type = "Slasher"
 
-DOCUMENT.Description = [[MONDAY NIGHT RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAW]]
-DOCUMENT.AdditionalDescription = [[MONDAY NIGHT RAAAAAAW]]
+DOCUMENT.Description = [[This demon creature shapes into the form of an action figure, holding an championship belt on his shoulder and a pair of cool glasses. He's also very attached to a dog plushie.]]
+DOCUMENT.AdditionalDescription = [[Reports says he's very aggressive and will try to attack everyone that take his dog plushie away. Awaiting more scout reports.]]
 
 SlashCo.RegisterDocument(DOCUMENT)
