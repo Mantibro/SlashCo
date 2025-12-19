eeca5a2b3d149a92c3539090c24ae8e7eeb5eb4c
Xerkdot <152566871+Xerkdot@users.noreply.github.com>
2025-12-19 20:34:16 -0300

    Borgmire: reworked his gameplay a bit
    [+] Implemented anger system, also adding an anger meter in his hud.
    [+] Added a new ability: Kick.
    -> This will make him move a lot faster for a few seconds and finish with a kick that can launch survivors a bit.
    [+] Implemented a prevously unused Stunned animation.
    [#] The ability Throw now requires 50% anger to be unlocked. each use of Throw will reduce the anger in 30%

--- lua/slashco/slasher/borgmire.lua
+++ lua/slashco/slasher/borgmire.lua
@@ -9,7 +9,7 @@ SLASHER.ID = 8
 SLASHER.Class = SlashCo.SlasherClass.Cryptid
 SLASHER.DangerLevel = SlashCo.DangerLevel.Devastating
 SLASHER.IsSelectable = true
-SLASHER.Model = "models/slashco/slashers/borgmire/borgmire.mdl"
+SLASHER.Model = "models/slashco/slashers/borgmire/borgmire2.mdl"
 SLASHER.GasCanMod = 0
 SLASHER.KillDelay = 0
 SLASHER.ProwlSpeed = 150
@@ -29,18 +29,23 @@ SLASHER.ProTip = "Borgmire_tip"
 SLASHER.SpeedRating = "★★★★☆"
 SLASHER.EyeRating = "★☆☆☆☆"
 SLASHER.DiffRating = "★☆☆☆☆"
+SLASHER.AngerIncrease = 10 -- Anger increase by punching and kicking people.
+SLASHER.AngerPassiveGain = 0
+SLASHER.AngerChaseGain = 0.01
 -- Balancement Vars
 SLASHER.PunchDamage = 35
+SLASHER.KickDamage = 25
 SLASHER.ThrowStrengthForward = 1600 -- forward Velocity used when throwing a player
 SLASHER.ThrowStrengthUp = 800 -- up Velocity used when throwing a player
 SLASHER.ChaseDecreaseMult = 14 -- Multiplier used when decreasing the chase duration
-SLASHER.PunchSlowdownDiv = 2 -- Used to divide FrameTime making the PunchSlowdown last longer, the lower, the shorter the Slowdown becomes. 
+SLASHER.PunchSlowdownDiv = 2 -- Used to divide FrameTime making the PunchSlowdown last longer, the lower, the shorter the Slowdown becomes.
 SLASHER.ChaseSpeedReduction = 0
 
 function SLASHER.OnBalanceForPlayers(totalSurvivors, additionalSurvivors)
 	local SO = SlashCo.CurRound.OfferingData.Singularity
 
 	SLASHER.PunchDamage = 35 + (SO * 20) + (1.5 * additionalSurvivors)
+	SLASHER.KickDamage = 25 + (SO * 20) + (1.5 * additionalSurvivors)
 	SLASHER.ThrowStrengthForward = 1600 + (SO * 450)  + (30 * additionalSurvivors)
 	SLASHER.ThrowStrengthUp = 800 + (SO * 150) + (10 * additionalSurvivors)
 	SLASHER.PunchSlowdownDiv = math.Clamp((SO / 2) - (0.05 * additionalSurvivors), 0.5, 2)
@@ -61,6 +66,7 @@ function SLASHER.OnSpawn(slasher)
 	slasher.PunchCooldown = 0
 	slasher.PunchSlowdown = 0
 	slasher.ThrowCooldown = 0
+	slasher.KickCooldown = 0
 end
 
 function SLASHER.OnTickBehaviour(slasher)
@@ -68,6 +74,7 @@ function SLASHER.OnTickBehaviour(slasher)
 	local PunchCD = slasher.PunchCooldown or 0 --Punch Cooldown
 	local PunchSD = slasher.PunchSlowdown or 0 --Punch Slowdown
 	local ThrowCD = slasher.ThrowCooldown or 0 -- Throw Cooldown
+	local KickCD = slasher.KickCooldown or 0 -- Kick Cooldown
 
 	if PunchCD > 0 then
 		slasher.PunchCooldown = PunchCD - FrameTime()
@@ -75,6 +82,10 @@ function SLASHER.OnTickBehaviour(slasher)
 	
 	if ThrowCD > 0 then
 		slasher.ThrowCooldown = ThrowCD - FrameTime()
+	end
+	
+	if KickCD > 0 then
+		slasher.KickCooldown = KickCD - FrameTime()
 	end
 
 	if PunchSD > 1 then
@@ -88,8 +99,13 @@ function SLASHER.OnTickBehaviour(slasher)
 	if not slasher:GetNWBool("InSlasherChaseMode") then
 		slasher.TimeChasing = 0
 
-		slasher:SetRunSpeed(SLASHER.ProwlSpeed)
-		slasher:SetWalkSpeed(SLASHER.ProwlSpeed)
+		if slasher:GetNWBool("BorgmireKick") then
+			slasher:SetRunSpeed(SLASHER.ProwlSpeed + 200)
+			slasher:SetWalkSpeed(SLASHER.ProwlSpeed + 200)
+		else
+			slasher:SetRunSpeed(SLASHER.ProwlSpeed)
+			slasher:SetWalkSpeed(SLASHER.ProwlSpeed)
+		end
 
 		slasher.ChaseSound = nil
 
@@ -108,8 +124,13 @@ function SLASHER.OnTickBehaviour(slasher)
 
 		slasher.TimeChasing = ChaseTime + FrameTime()
 
-		slasher:SetRunSpeed((SLASHER.ChaseSpeed - math.sqrt(ChaseTime * (14 - SLASHER.ChaseSpeedReduction))) / PunchSD)
-		slasher:SetWalkSpeed((SLASHER.ChaseSpeed - math.sqrt(ChaseTime * (14 - SLASHER.ChaseSpeedReduction))) / PunchSD)
+		if slasher:GetNWBool("BorgmireKick") then
+			slasher:SetRunSpeed(SLASHER.ChaseSpeed + 200)
+			slasher:SetWalkSpeed(SLASHER.ChaseSpeed + 200)
+		else
+			slasher:SetRunSpeed((SLASHER.ChaseSpeed - math.sqrt(ChaseTime * (14 - SLASHER.ChaseSpeedReduction))) / PunchSD)
+			slasher:SetWalkSpeed((SLASHER.ChaseSpeed - math.sqrt(ChaseTime * (14 - SLASHER.ChaseSpeedReduction))) / PunchSD)
+		end
 
 		if slasher.ChaseSound == nil then
 			slasher:PlayGlobalSound("slashco/slasher/borgmire/borgmire_breath_chase.mp3", 70, nil, true)
@@ -123,6 +144,21 @@ function SLASHER.OnTickBehaviour(slasher)
 
 			slasher.ChaseSound = true
 		end
+	end
+	
+	local anger = SlashCo.GetSlasherAnger(slasher)
+	if anger < 50 then
+		slasher:SetNWBool("CanThrow", false)
+	else
+		slasher:SetNWBool("CanThrow", true)
+	end
+	
+	if slasher:GetNWBool("InSlasherChaseMode") then
+		SlashCo.AddSlasherAnger(slasher, SLASHER.AngerChaseGain)
+	end
+	
+	if slasher:GetNWInt("BorgmireAnger") ~= math.floor(anger) then
+		slasher:SetNWInt("BorgmireAnger", math.floor(anger))
 	end
 
 	slasher:SetEyeSight(SLASHER.Eyesight)
@@ -170,6 +206,7 @@ function SLASHER.OnPrimaryFire(slasher)
 				util.Effect("BloodImpact", bloodfx)
 
 				target:EmitSound("slashco/slasher/borgmire/borgmire_hit" .. math.random(1, 2) .. ".mp3")
+				SlashCo.AddSlasherAnger(slasher, SLASHER.AngerIncrease)
 			end
 		end)
 
@@ -196,7 +233,70 @@ function SLASHER.OnSecondaryFire(slasher)
 	SlashCo.StartChaseMode(slasher)
 end
 
+function SLASHER.OnMainAbilityFire(slasher)
+	if slasher:GetNWBool("BorgmireThrow") then
+		return
+	end
+
+	if slasher.BorgPunching then
+		return
+	end
+
+	if slasher.KickCooldown < 0.01 then
+		slasher:SetNWBool("BorgmireKick", false)
+		timer.Remove("BorgmireKickDecay")
+		slasher.KickCooldown = 7
+		
+		timer.Simple(2.0, function()
+			slasher:EmitSound("slashco/slasher/borgmire/borgmire_swing" .. math.random(1, 2) .. ".mp3")
+			
+			local target = slasher:TraceHullAttack(slasher:EyePos(), slasher:LocalToWorld(Vector(50, 0, 50)),
+				Vector(-35, -45, -60), Vector(35, 45, 60), SLASHER.KickDamage, DMG_SLASH, 5, false)
+
+			if not target:IsValid() then
+				return
+			end
+			
+			SlashCo.BustDoor(slasher, target, 60000)
+			
+			if (target:IsPlayer() and target:Team() == TEAM_SURVIVOR) or target:GetClass() == "prop_ragdoll" then
+				local o = Vector(0, 0, 0)
+
+				if (target:IsPlayer() and target:Team() == TEAM_SURVIVOR) then
+					o = Vector(0, 0, 50)
+				end
+
+				local vPoint = target:GetPos() + o
+				local bloodfx = EffectData()
+				bloodfx:SetOrigin(vPoint)
+				util.Effect("BloodImpact", bloodfx)
+
+				target:EmitSound("slashco/slasher/borgmire/borgmire_hit" .. math.random(1, 2) .. ".mp3")
+				target:SetVelocity((slasher:GetForward() * 600) + Vector(0, 0, 400))
+
+				SlashCo.AddSlasherAnger(slasher, SLASHER.AngerIncrease)
+			end
+		end)
+		
+		timer.Simple(0.1, function()
+			if not IsValid(slasher) then
+				return
+			end
+			
+			slasher:SetNWBool("BorgmireKick", true)
+
+			timer.Create("BorgmireKickDecay", 2.1, 1, function()
+				slasher:SetNWBool("BorgmireKick", false)
+			end)
+		end)
+	end
+end
+
 function SLASHER.OnSpecialAbilityFire(slasher, target)
+	if not slasher:GetNWBool("CanThrow") then
+		return
+	end
+
 	if slasher.BorgPunching then
 		return
 	end
@@ -245,6 +345,7 @@ function SLASHER.OnSpecialAbilityFire(slasher, target)
 			end
 			
 			slasher.ThrowCooldown = 3
+			SlashCo.AddSlasherAnger(slasher, -30)
 		end)
 
 		timer.Simple(2, function()
@@ -258,22 +359,41 @@ function SLASHER.OnSpecialAbilityFire(slasher, target)
 	end
 end
 
+function SLASHER.OnHitByTeslaCoil(slasher)
+	SlashCo.StopChase(slasher)
+
+	slasher:SetNWBool("BorgmireStunned", true)
+	slasher:Freeze(true)
+	timer.Simple(16, function()
+		if not IsValid(slasher) then return end
+
+		slasher:SetNWBool("BorgmireStunned", false)
+		slasher:Freeze(false)
+	end)
+end
+
 function SLASHER.Animator(ply)
 	local chase = ply:GetNWBool("InSlasherChaseMode")
 	local borg_punch = ply:GetNWBool("BorgmirePunch")
 	local borg_throw = ply:GetNWBool("BorgmireThrow")
+	local borg_kick = ply:GetNWBool("BorgmireKick")
+	local borg_stun = ply:GetNWBool("BorgmireStunned")
 
-	if not borg_punch and not borg_throw then
+	if not borg_punch and not borg_throw and not borg_kick then
 		ply.anim_antispam = false
 	end
 
 	if ply:IsOnGround() then
-		if not chase then
-			ply.CalcIdeal = ACT_HL2MP_WALK
-			ply.CalcSeqOverride = ply:LookupSequence("walk_all")
+		if borg_stun then
+			ply.CalcSeqOverride = ply:LookupSequence("sit")
 		else
-			ply.CalcIdeal = ACT_HL2MP_RUN
-			ply.CalcSeqOverride = ply:LookupSequence("run_all")
+			if not chase then
+				ply.CalcIdeal = ACT_HL2MP_WALK
+				ply.CalcSeqOverride = ply:LookupSequence("walk_all")
+			else
+				ply.CalcIdeal = ACT_HL2MP_RUN
+				ply.CalcSeqOverride = ply:LookupSequence("run_all")
+			end
 		end
 	else
 		ply.CalcSeqOverride = ply:LookupSequence("jump")
@@ -294,6 +414,19 @@ function SLASHER.Animator(ply)
 
 	if borg_throw and (ply.anim_antispam == nil or ply.anim_antispam == false) then
 		ply:AddVCDSequenceToGestureSlot(1, ply:LookupSequence("attack_throw"), 0, true)
+		ply.anim_antispam = true
+	end
+	
+	if borg_kick and (ply.anim_antispam == nil or ply.anim_antispam == false) then
+		local rand2 = math.random(1, 2)
+		local KickAnim = ""
+		if rand2 == 1 then
+			KickAnim = "charge_attack_1"
+		else
+			KickAnim = "charge_attack_2"
+		end
+
+		ply:AddVCDSequenceToGestureSlot(1, ply:LookupSequence(KickAnim), 0, true)
 		ply.anim_antispam = true
 	end
 
@@ -333,7 +466,22 @@ function SLASHER.InitHud(_, hud)
 
 	hud:AddControl("LMB", "punch", Material("slashco/ui/icons/slasher/s_punch"))
 	hud:ChaseAndKill(nil, true)
+	hud:AddControl("R", "kick", Material("slashco/ui/icons/slasher/s_kick"))
 	hud:AddControl("F", "throw", Material("slashco/ui/icons/slasher/s_punch"))
+	
+	hud:AddMeter("anger", 100, "", nil, true)
+	hud:TieMeterInt("anger", "BorgmireAnger")
+
+	function hud.AlsoThink()
+		local canThrow = GameData.LocalPlayer:GetNWBool("CanThrow")
+		if not canThrow then
+			hud:SetControlEnabled("F", false)
+			hud:SetControlVisible("F", false)
+		else
+			hud:SetControlEnabled("F", true)
+			hud:SetControlVisible("F", true)
+		end
+	end
 end
 
 SlashCo.RegisterSlasher(SLASHER, "Borgmire")
