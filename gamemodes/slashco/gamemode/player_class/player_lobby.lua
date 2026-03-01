56844f7e8cceafe1f6f02d48728a1388446150e7
Xerkdot <152566871+Xerkdot@users.noreply.github.com>
2026-02-28 22:26:03 -0300

    gamemode: make survivor models work in stage 0

--- gamemodes/slashco/gamemode/player_class/player_lobby.lua
+++ gamemodes/slashco/gamemode/player_class/player_lobby.lua
@@ -19,6 +19,30 @@ function PLAYER:Loadout()
 	self.Player:RemoveAllAmmo()
 end
 
+SlashCo.SurvivorModels = file.Find("models/slashco/survivor/male_*.mdl", "GAME")
+for idx, fileName in ipairs(SlashCo.SurvivorModels) do
+	SlashCo.SurvivorModels[idx] = "models/slashco/survivor/" .. fileName
+	SlashCo.SurvivorModels[SlashCo.SurvivorModels[idx]] = idx
+end
+
+hook.Add("SlashCo:Precache", "PrecacheSurvivorModels", function()
+	for _, modelName in ipairs(SlashCo.SurvivorModels) do
+		SlashCo.PrecacheModel(modelName)
+	end
+end)
+
+function PLAYER:SetModel()
+	local modelname
+	local cl_modelname = self.Player:GetInfo("slashco_cl_playermodel")
+	if SlashCo.SurvivorModels[cl_modelname] then
+		modelname = cl_modelname
+	else
+		modelname = SlashCo.SurvivorModels[math.random(1, #SlashCo.SurvivorModels)]
+	end
+
+	self.Player:SetModel(modelname)
+end
+
 function PLAYER:SetupDataTables()
 	if SERVER then
 		--PLAYER.Achievements = data.Stats.Achievements or {}
